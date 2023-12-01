f9ccb64cdf5fc2e0b02ed2a13ba06a3b1232ebb8
yuhong000 <s10258441@connect.np.edu.sg>
2023-12-01 22:53:42 +0800

    12/1

--- java.js
+++ java.js
@@ -1,8 +1,18 @@
-const bar= document.getElementById("bar");
-const nav= document.getElementById("navbar");
-
-if (bar){
-    bar.addEventListener("click",()=>{
-        nav.classList.add("active");
-    })
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const bar = document.getElementById('bar');
+    const nav = document.getElementById('navbar');
+    const close = document.getElementById('close');
+  
+    if (bar) {
+      bar.addEventListener('click', () => {
+        nav.classList.toggle('active');
+      });
+    }
+  
+    if (close) {
+      close.addEventListener('click', () => {
+        nav.classList.remove('active');
+      });
+    }
+  });
+  
